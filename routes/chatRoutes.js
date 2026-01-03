0412aa1bd805d9ad0600046344ed1d0dcdaff4e3
MostafaLabib21 <labibmostafa1971@gmail.com>
2026-01-03 19:02:34 +0600

    Fixed the error

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -79,7 +79,7 @@ router.post('/', async (req, res) => {
     `;
 
     // 4. Call the AI
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+   const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
     
     const result = await model.generateContent(context);
     const response = await result.response;
